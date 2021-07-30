bc13513e4266936e5dcff3bce72327c0aa0c991e
rajachandran1 <rchandran@sirahu.com>
2021-07-30 12:06:35 +0530

    Added responsive header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { LocationPickerComponent } from './location-picker/location-picker.component';
+import { HeaderComponent } from './header/header.component';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 
 @NgModule({
-  declarations: [LocationPickerComponent],
-  imports: [CommonModule, IonicModule],
-  exports: [LocationPickerComponent],
+  declarations: [LocationPickerComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, IonicModule],
+  exports: [LocationPickerComponent, HeaderComponent],
   providers: [
     Geolocation,
     LocationAccuracy,
